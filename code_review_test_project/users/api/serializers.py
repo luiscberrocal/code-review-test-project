4ee79070b8c5c6846e0abb05d158a4bf5bd23314
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2025-10-05 09:05:55 -0500

    First commit

---
+++ code_review_test_project/users/api/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+from code_review_test_project.users.models import User
+
+
+class UserSerializer(serializers.ModelSerializer[User]):
+    class Meta:
+        model = User
+        fields = ["username", "name", "url"]
+
+        extra_kwargs = {
+            "url": {"view_name": "api:user-detail", "lookup_field": "username"},
+        }
